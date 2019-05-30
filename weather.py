89cb3abca3a1a7e7c82a0cfe49bbd5283bf626f7
Julia Torrejon <juliaesther.torrejon@gmail.com>
2019-05-30 23:12:04 +0100

    Update weather.py

--- weather.py
+++ weather.py
@@ -21,11 +21,11 @@ from datetime import datetime
 from decimal import *
 
 # Convert temperature from Kelvin to Degrees Celsius - [°C] = [K] − 273.15
-def temperature_kelvin_to_celsius(temp_in_kelvin):
+def temp_kelvin_to_celsius(temp_in_kelvin):
     return decimal(temp_in_kelvin) - decimal(273.15)
 
 # API Call & Parameters requested from the API response
-def fetch_weather_(city_name, country_code, appid)
+def fetch_weather(city_name, country_code, appid)
 
 # Build URL query depending on if there is a country or not - It restricts cities to an ISO 3166 country code
     if country_code:
@@ -34,20 +34,27 @@ def fetch_weather_(city_name, country_code, appid)
         query = {"q":city_name, "appid":api_key}
 
 # Get data from openweathermap.org & passing parameter in URL
-r = requests.get(‘api.openweathermap.org/data/2.5/weather’, params=query)
+request = requests.get(‘api.openweathermap.org/data/2.5/weather’, params=query)
 
 # Check that the URL has been correctly encoded by printing the URL
 print(r.url)
 
 # Check the HTTP response status code - If there is a staus_code = 200 - then we have a valid response
-r.status_code
+status = r.status_code
 
 # In case the JSON decoding fails, r.json() raises an exception
-r.json()
+response = r.json()
 
 # Print reponse
-
-
+temperature = temp_kelvin_to_celsius(response["main"]["temp"])
+max_temperature = temp_kelvin_to_celsius(response["main"]["temp_max"])
+min_temperature = temp_kelvin_to_celsius(response["main"]["temp_min"])
+humidity = response["main"]["humidity"]
+clouds = response["all"]["clouds"]
+rain = response["h1"]["rain"]
+sunrise = response["sys"]["sunrise"]
+sunset = response["sys"]["sunset"]
+country =  response["sys"]["country"]
 
 # The option parser is generated based on the docstring above that is passed to docopt function
 if __name__ == '__main__':
