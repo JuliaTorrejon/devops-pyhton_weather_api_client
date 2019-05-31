80c6db040eb6ac7300a85c2800bc5dd5faddca29
Julia Torrejon <juliaesther.torrejon@gmail.com>
2019-05-31 18:46:43 +0100

    Update weather.py

--- weather.py
+++ weather.py
@@ -45,20 +45,32 @@ status = r.status_code
 # If HTTP status code is = 200 (Valid response)
 if r.status_code == 200:
 response = r.json()
-
 # Print (response)
-# Print (response)[]
-temperature = temp_kelvin_to_celsius(response["main"]["temp"])
-max_temperature = temp_kelvin_to_celsius(response["main"]["temp_max"])
-min_temperature = temp_kelvin_to_celsius(response["main"]["temp_min"])
-humidity = response["main"]["humidity"]
-clouds = response["all"]["clouds"]
-rain = response["h1"]["rain"]
-sunrise = response["sys"]["sunrise"]
-sunset = response["sys"]["sunset"]
-country = response["sys"]["country"]
-
-
+# Print (response)["main"]["temperature"]
+      temperature = temp_kelvin_to_celsius(response["main"]["temp"])
+      max_temperature = temp_kelvin_to_celsius(response["main"]["temp_max"])
+      min_temperature = temp_kelvin_to_celsius(response["main"]["temp_min"])
+      humidity = response["main"]["humidity"]
+      clouds = response["all"]["clouds"]
+      rain = response["h1"]["rain"]
+      sunrise = response["sys"]["sunrise"]
+      sunset = response["sys"]["sunset"]
+      country = response["sys"]["country"]
+      json = response
+# Otherwise if there is an error in the status code then return status code and blank temperature
+else:
+      status = r.status_code
+      temperature = ""
+      max_temperature = "" 
+      min_temperature = ""
+      humidity = ""
+      clouds = ""
+      rain = ""
+      sunrise = "" 
+      sunset = ""
+      country = ""
+      json = ""
+      
 # The option parser is generated based on the docstring above that is passed to docopt function
 if __name__ == '__main__':
     arguments = docopt(__doc__)
